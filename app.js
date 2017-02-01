416d136848b9e38847ba4fcae1149e019f5802e1
Rishikesh Dhokare <rishikesh.m.dhokare@gmail.com>
2017-02-01 17:50:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+// first api which just responds to the request without any processing
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
+// second api which waits for 50 millis before responding 
 app.get('/heavy-ping', (req, res) => {
     setTimeout(() => {
         console.info("eating time for demo");
